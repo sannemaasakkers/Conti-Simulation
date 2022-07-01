17e591f2beac542f9e1150c3fbd64c6cf02917f6
adversae <sanne@adversae.nl>
2022-07-01 05:42:33 -0700

    Delete old file

--- leaks-website/args.py
+++
@@ -1,7 +0,0 @@
-import argparse
-
-parser = argparse.ArgumentParser(description="Create ContiLeaks demo website",
-                                 formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-parser.add_argument("-n", "--name", help="Name of the hacked organisation")
-parser.add_argument("-d", "--dir",  help="Directory of the stolen files")
-args = parser.parse_args()
